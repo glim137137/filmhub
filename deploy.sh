38bda8b544481808244282ee2911b69050a6ef51
glim <glim137137@gmail.com>
2025-12-29 01:41:45 +0800

    modify(deploy.py, requirements.txt, app.py)

--- deploy.sh
+++ deploy.sh
@@ -1,21 +1,20 @@
 #!/bin/bash
 
+echo "Install python 3.12..."
+
+pip install --upgrade pip
+pip install -r requirements.txt
+
 cd ./api/src
 echo "Installing database..."
 python -u init_db.py
 echo "Running app.py..."
-gunicorn -w 4 -k gevent -b 0.0.0.0:5000 --access-logfile data/logs/access.log --error-logfile data/logs/error.log app:app
+gunicorn -w 4 -k gevent -b 0.0.0.0:5000 --access-logfile ./data/logs/access.log --error-logfile data/logs/error.log app:app
 
 # Install Node.js environment
 echo "Installing Node.js..."
-curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.0/install.sh | bash
-source ~/.bashrc
-
-nvm install node
-node --version
-npm --version
 
 cd ../../web
 npm install
 echo "Running vue.js..."
-npm run dev
+npm run build
